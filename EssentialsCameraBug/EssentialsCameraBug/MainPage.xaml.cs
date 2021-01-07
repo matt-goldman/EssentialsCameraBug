2ec50effa2809e801170ee941b24c959651dec44
Matt Goldman <mattgoldman1981@gmail.com>
2021-01-07 18:04:46 +1100

    built demo

---
+++ EssentialsCameraBug/EssentialsCameraBug/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EssentialsCameraBug
+{
+    public partial class MainPage : ContentPage
+    {
+        private MainViewModel ViewModel { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            ViewModel = new MainViewModel();
+            BindingContext = ViewModel;
+        }
+    }
+}
---
+++ EssentialsCameraBug/EssentialsCameraBug/MainViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace EssentialsCameraBug
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ICommand TakePictureCommand => new Command(async () => await TakePicture());
+
+        public ImageSource CapturedPicture { get; set; } = ImageSource.FromFile("");
+
+        public string FilePath { get; set; }
+
+        public MainViewModel()
+        {
+
+        }
+
+
+        private async Task TakePicture()
+        {
+            var status = await Permissions.CheckStatusAsync<Permissions.Camera>();
+
+            if(status != PermissionStatus.Granted)
+            {
+                return;
+            }
+
+            var photo = await MediaPicker.CapturePhotoAsync();
+
+            CapturedPicture = ImageSource.FromFile(photo.FullPath);
+            FilePath = photo.FullPath;
+            RaisePropertyChanged(nameof(CapturedPicture));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void RaisePropertyChanged(params string[] properties)
+        {
+            foreach (var propertyName in properties)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
